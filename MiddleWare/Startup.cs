e0561cafeb0262aebcbdecb32acbe80c5d851ecb
aneesha003 <aneesha05@gmail.com>
2017-12-15 15:11:55 +0530

    created middleware project

---
+++ MiddleWare/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace MiddleWare
+{
+public class Startup
+{
+    private static void HandleMapTest1(IApplicationBuilder app)
+    {
+        app.Run(async context =>
+        {
+            await context.Response.WriteAsync("hallo...");
+        });
+    }
+
+    private static void HandleMapTest2(IApplicationBuilder app)
+    {
+        app.Run(async context =>
+        {
+            await context.Response.WriteAsync("InterFantacode");
+        });
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        app.Map("/map1", HandleMapTest1);
+
+        app.Map("/map2", HandleMapTest2);
+
+        app.Run(async context =>
+        {
+            await context.Response.WriteAsync("Hello from non-Map delegate. <p>");
+        });
+    }
+}}
